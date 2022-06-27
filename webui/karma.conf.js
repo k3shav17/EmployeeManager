f6f258a82d14bc00f38986c46d95c3a3cc9b61ef
k3shav17 <keshavrao0489@gmail.com>
2022-06-27 14:48:15 +0530

    removed previous structure due to some errors

--- webui/karma.conf.js
+++ webui/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/employeemanagerapp'),
+      dir: require('path').join(__dirname, './coverage/webui'),
       subdir: '.',
       reporters: [
         { type: 'html' },
